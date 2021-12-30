ef4f18df4ae608af8446467c5d76ed60dcfd07ed
Modamori Oluwayomi <77457879+Portia-dot@users.noreply.github.com>
2021-12-30 12:43:28 -0600

    Update
    Update API

--- src/services/Services.js
+++ src/services/Services.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: 'https://restcountries.eu/rest/v2',
+  baseURL: 'https://restcountries.com/v2',
   withCredentials: false,
   headers: {
     Accept: 'application/json',
